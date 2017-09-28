27921b0d8a1cd3ebc578131c17bfe34996d98e52
Jose-Pc <josemariaplanelles.alu@iespacomolla.es>
2017-09-28 08:56:04 +0200

    prueba8

--- app/src/main/java/com/example/alumno/lawasap/MainActivity.java
+++ app/src/main/java/com/example/alumno/lawasap/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_chat);
+        setContentView(R.layout.layout_configuracion);
     }
 }
